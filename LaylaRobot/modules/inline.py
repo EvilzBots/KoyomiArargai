21f2a341bb13398e69128bc85c9fdbd600f7c86a
EvilzBots <85481867+EvilzBots@users.noreply.github.com>
2021-06-11 22:51:09 +0530

    Update inline.py

--- LaylaRobot/modules/inline.py
+++ LaylaRobot/modules/inline.py
@@ -37,150 +37,46 @@ from LaylaRobot import telethn as YoneTelethonClient, TIGERS, DRAGONS, DEMONS
 
 app = tbot
 
-@app.on(events.InlineQuery(pattern=r"info"))
-def info(update: Update, context: CallbackContext):
-    bot, args = context.bot, context.args
-    message = update.effective_message
-    chat = update.effective_chat
-    user_id = extract_user(update.effective_message, args)
-
-    if user_id:
-        user = bot.get_chat(user_id)
-
-    elif not message.reply_to_message and not args:
-        user = message.from_user
-
-    elif not message.reply_to_message and (
-        not args
-        or (
-            len(args) >= 1
-            and not args[0].startswith("@")
-            and not args[0].isdigit()
-            and not message.parse_entities([MessageEntity.TEXT_MENTION])
-        )
-    ):
-        message.reply_text("I can't extract a user from this.")
-        return
-
-    else:
-        return
-
-    rep = message.reply_text("<code>Appraising...</code>", parse_mode=ParseMode.HTML)
-
-    text = (
-        f"╒═══「<b> Appraisal results:</b> 」\n"
-        f"ID: <code>{user.id}</code>\n"
-        f"First Name: {html.escape(user.first_name)}"
-    )
-
-    if user.last_name:
-        text += f"\nLast Name: {html.escape(user.last_name)}"
-
-    if user.username:
-        text += f"\nUsername: @{html.escape(user.username)}"
-
-    text += f"\nPermalink: {mention_html(user.id, 'link')}"
-
-    if chat.type != "private" and user_id != bot.id:
-        _stext = "\nPresence: <code>{}</code>"
-
-        afk_st = is_afk(user.id)
-        if afk_st:
-            text += _stext.format("AFK")
-        else:
-            status = status = bot.get_chat_member(chat.id, user.id).status
-            if status:
-                if status in {"left", "kicked"}:
-                    text += _stext.format("Not here")
-                elif status == "member":
-                    text += _stext.format("Detected")
-                elif status in {"administrator", "creator"}:
-                    text += _stext.format("Admin")
-    if user_id not in [bot.id, 777000, 1087968824]:
-        userhp = hpmanager(user)
-        text += f"\n\n<b>Health:</b> <code>{userhp['earnedhp']}/{userhp['totalhp']}</code>\n[<i>{make_bar(int(userhp['percentage']))} </i>{userhp['percentage']}%]"
-
+@app.on(events.InlineQuery(pattern=r"bin"))
+async def hehe(event):
     try:
-        spamwtc = sw.get_ban(int(user.id))
-        if spamwtc:
-            text += "\n\n<b>This person is Spamwatched!</b>"
-            text += f"\nReason: <pre>{spamwtc.reason}</pre>"
-            text += "\nAppeal at @SpamWatchSupport"
-        else:
-            pass
-    except:
-        pass  # don't crash if api is down somehow...
+        input = event.text.split(" ", maxsplit=1)[1]
+    except IndexError:
+        lund = event.builder.article(
+           title="Search Something",
+           description="You haven't searched anything",
+           text="**Bɪɴ Cʜᴇᴄᴋᴇʀ**\nYou haven't given anything to check",
+           buttons=Button.switch_inline("Sᴇᴀʀᴄʜ Aɢᴀɪɴ", query="bin ", same_peer=True)
+           )
+        await event.answer([lund])
+    input = event.text.split(" ", maxsplit=1)[1]
+    url = requests.get(f"https://bins-su-api.now.sh/api/{input}")
+    res = url.json()
+    vendor = res['data']['vendor']
+    type = res['data']['type']
+    level = res['data']['level']
+    bank = res['data']['bank']
+    country = res['data']['country']
 
-    disaster_level_present = False
-
-    if user.id == OWNER_ID:
-        text += "\n\nThe Disaster level of this person is 'God'."
-        disaster_level_present = True
-    elif user.id in DEV_USERS:
-        text += "\n\nThis user is member of 'Hero Association'."
-        disaster_level_present = True
-    elif user.id in DRAGONS:
-        text += "\n\nThe Disaster level of this person is 'Dragon'."
-        disaster_level_present = True
-    elif user.id in DEMONS:
-        text += "\n\nThe Disaster level of this person is 'Demon'."
-        disaster_level_present = True
-    elif user.id in TIGERS:
-        text += "\n\nThe Disaster level of this person is 'Tiger'."
-        disaster_level_present = True
-    elif user.id in WOLVES:
-        text += "\n\nThe Disaster level of this person is 'Wolf'."
-        disaster_level_present = True
-
-    if disaster_level_present:
-        text += ' [<a href="https://t.me/OnePunchUpdates/155">?</a>]'.format(
-            bot.username
-        )
-
-    try:
-        user_member = chat.get_member(user.id)
-        if user_member.status == "administrator":
-            result = requests.post(
-                f"https://api.telegram.org/bot{TOKEN}/getChatMember?chat_id={chat.id}&user_id={user.id}"
-            )
-            result = result.json()["result"]
-            if "custom_title" in result.keys():
-                custom_title = result["custom_title"]
-                text += f"\n\nTitle:\n<b>{custom_title}</b>"
-    except BadRequest:
-        pass
-
-    for mod in USER_INFO:
-        try:
-            mod_info = mod.__user_info__(user.id).strip()
-        except TypeError:
-            mod_info = mod.__user_info__(user.id, chat.id).strip()
-        if mod_info:
-            text += "\n\n" + mod_info
-
-    if INFOPIC:
-        try:
-            profile = context.bot.get_user_profile_photos(user.id).photos[0][-1]
-            _file = bot.get_file(profile["file_id"])
-            _file.download(f"{user.id}.png")
-
-            message.reply_document(
-                document=open(f"{user.id}.png", "rb"),
-                caption=(text),
-                parse_mode=ParseMode.HTML,
-                disable_web_page_preview=True,
-            )
-
-            os.remove(f"{user.id}.png")
-        # Incase user don't have profile pic, send normal text
-        except IndexError:
-            message.reply_text(
-                text, parse_mode=ParseMode.HTML, disable_web_page_preview=True
-            )
-
-    else:
-        message.reply_text(
-            text, parse_mode=ParseMode.HTML, disable_web_page_preview=True
-        )
-
-    rep.delete()
+    me = (await event.client.get_me()).username
+    valid = f"""
+<b>➤ Valid Bin:</b>
+<b>Bin -</b> <code>{input}</code>
+<b>Status -</b> <code>Valid Bin</code>
+<b>Vendor -</b> <code>{vendor}</code>
+<b>Type -</b> <code>{type}</code>
+<b>Level -</b> <code>{level}</code>
+<b>Bank -</b> <code>{bank}</code>
+<b>Country -</b> <code>{country}</code>
+<b>Checked By - @{me}</b>
+<b>User-ID - {event.sender_id}</b>
+"""
+    Binning = event.builder.article(
+           title="Valid Bin",
+           description="It's a valid bin",
+           text=valid,
+           thumb=InputWebDocument(url="https://telegra.ph/file/0788aaa1b0b48e8e9e2f8.jpg", size=0, mime_type="image/jpeg", attributes=[]),
+           parse_mode="HTML",
+           buttons=[[Button.switch_inline("Sᴇᴀʀᴄʜ Aɢᴀɪɴ", query="bin ", same_peer=True), Button.switch_inline("Sʜᴀʀᴇ", query=f"bin {input}")]
+           ])
+    await event.answer([Binning])
