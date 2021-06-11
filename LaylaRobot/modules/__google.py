b4c61568d804af406fd14f17b616e0d0aaaaf80e
EvilzBots <85481867+EvilzBots@users.noreply.github.com>
2021-06-11 11:14:21 +0530

    Update __google.py

--- LaylaRobot/modules/__google.py
+++ LaylaRobot/modules/__google.py
@@ -64,7 +64,7 @@ async def _(event):
 async def img_sampler(event):
     if event.fwd_from:
         return
-    
+    webevent = await event.reply("searching........")
     query = event.pattern_match.group(1)
     jit = f'"{query}"'
     downloader.download(
@@ -275,16 +275,16 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "◎Search"
+__mod_name__ = "✘ Search ✘"
 
 __help__ = """
- ❍ /google <text>*:* Perform a google search
- ❍ /img <text>*:* Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
- ❍ /app <appname>*:* Searches for an app in Play Store and returns its details.
- ❍ /reverse: Does a reverse image search of the media which it was replied to.
- ❍ /gps <location>*:* Get gps location.
- ❍ /github <username>*:* Get information about a GitHub user.
- ❍ /country <country name>*:* Gathering info about given country
+ ➤ `/google` <text>*:* *Perform a google search*
+ ➤ `/img` <text>*:* *Search Google for images and returns them\nFor greater no. of results specify lim, For eg:* `/img hello lim=10`
+ ➤ `/app` <appname>*:* *Searches for an app in Play Store and returns its details.*
+ ➤ `/reverse`: *Does a reverse image search of the media which it was replied to.*
+ ✖ `/gps` <location>*:* *Get gps location.*
+ ✖ `/github` <username>*:* *Get information about a GitHub user.*
+ ✖ `/country` <country name>*:* *Gathering info about given country*
  ❍ /imdb <Movie name>*:* Get full info about a movie with imdb.com
  ❍ Yone <query>*:* Yone answers the query
   💡Ex: `Yone where is India?`
