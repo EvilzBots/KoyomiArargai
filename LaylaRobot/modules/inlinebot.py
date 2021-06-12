6879c604995a6316a5df889b64287159c01f6736
EvilzBots <85481867+EvilzBots@users.noreply.github.com>
2021-06-12 21:00:45 +0530

    Update inlinebot.py

--- LaylaRobot/modules/inlinebot.py
+++ LaylaRobot/modules/inlinebot.py
@@ -1,89 +1,17 @@
-import importlib
-import time
 import re
-from sys import argv
-from typing import Optional
-from LaylaRobot.modules import ALL_MODULES
-from LaylaRobot.modules.helper_funcs.chat_status import is_user_admin
-from LaylaRobot.modules.helper_funcs.misc import paginate_modules
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
-from telegram.error import (
-    BadRequest,
-    ChatMigrated,
-    NetworkError,
-    TelegramError,
-    TimedOut,
-    Unauthorized,
-)
-from telegram.ext import (
-    CallbackContext,
-    CallbackQueryHandler,
-    CommandHandler,
-    Filters,
-    MessageHandler,
-)
-from telegram.ext.dispatcher import DispatcherHandlerStop, run_async
-from telegram.utils.helpers import escape_markdown
+import random
+from LaylaRobot import tbot as Evil
+from telethon import Button
+from telethon import events
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import KeyboardButtonCallback
 
-IN_START_TEXT = """
-*Welcome To Inline Commands Menu:*
-"""
-
-buttons = [
-    [
-        InlineKeyboardButton(
-            text="✘ Anime ✘", callback_data="inlinebot"),
-    ],
-
-    
-    [
-        InlineKeyboardButton(text="✘ Back Me ✘", callback_data="help_back"),
-    ],
-]
-
-@run_async
-def anime_inline(update: Update, context: CallbackContext):
-    args = context.args
-    uptime = get_readable_time((time.time() - StartTime))
-    if update.effective_chat.type == "private":
-        if len(args) >= 1:
-            if args[0].lower() == "help":
-                send_help(update.effective_chat.id, HELP_STRINGS)
-            elif args[0].lower().startswith("ghelp_"):
-                mod = args[0].lower().split("_", 1)[1]
-                if not HELPABLE.get(mod, False):
-                    return
-                send_help(
-                    update.effective_chat.id,
-                    HELPABLE[mod].__help__,
-                    InlineKeyboardMarkup(
-                        [[InlineKeyboardButton(text="⬅️ BACK", callback_data="help_back")]]
-                    ),
-                )
-
-            elif args[0].lower().startswith("stngs_"):
-                match = re.match("stngs_(.*)", args[0].lower())
-                chat = dispatcher.bot.getChat(match.group(1))
-
-                if is_user_admin(chat, update.effective_user.id):
-                    send_settings(match.group(1), update.effective_user.id, False)
-                else:
-                    send_settings(match.group(1), update.effective_user.id, True)
-
-            elif args[0][1:].isdigit() and "rules" in IMPORTED:
-                IMPORTED["rules"].send_rules(update, args[0], from_pm=True)
-
-        else:
-            update.effective_message.reply_text(
-                IN_START_TEXT,
-                reply_markup=InlineKeyboardMarkup(buttons),
-                parse_mode=ParseMode.MARKDOWN,
-                timeout=60,
-            )
-    else:
-        update.effective_message.reply_text(
-            "I'm awake already!\n<b>Haven't slept since:</b> <code>{}</code>".format(
-                uptime
-            ),
-            parse_mode=ParseMode.HTML,
-        )
+IN_TXT = '''
+Here is the help for the Anime Lovers ❤️:
+Get information about anime, manga or characters from AniList.
+Available commands:
+ ➤ `/YONE` <anime>: returns information about the anime.
+'''
+@Evil.on(events.callbackquery.CallbackQuery(data="inlinebot"))
+async def inlinebot(event):
+    await event.edit(IN_TXT)
