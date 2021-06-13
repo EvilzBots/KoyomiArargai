31b972755d5110a1a38e4c656107b17c89a356f3
EvilzBots <85481867+EvilzBots@users.noreply.github.com>
2021-06-13 11:18:44 +0530

    Update inlinebot.py

--- LaylaRobot/modules/inlinebot.py
+++ LaylaRobot/modules/inlinebot.py
@@ -16,5 +16,4 @@ IN_TXT = '''
 async def inlinebot(event):
     await event.edit(IN_TXT, buttons=[
         [Button.switch_inline("✘ Anime ✘ ", query="anime", same_peer=True), Button.switch_inline("✘ Manga ✘", query="manga", same_peer=True)],
-        [Button.switch_inline("✘ Character Info ✘", query="character", same_peer=True)],
         [Button.inline("Next", data="help_back")]]
