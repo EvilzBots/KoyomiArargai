49e3a0dcfa04bf7487a750eb0939421befd779b8
EvilzBots <85481867+EvilzBots@users.noreply.github.com>
2021-06-14 19:21:37 +0530

    Update logomaker.py

--- LaylaRobot/modules/logomaker.py
+++ LaylaRobot/modules/logomaker.py
@@ -99,11 +99,11 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "LogoByLayla.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @LaylaList Support @AwesomeSupport")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @MrKoyomiBot Support @KoyomiSupport")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @AwesomeSupport, {e}')
+   await event.reply(f'Error Report @KoyomiSupport, {e}')
 
 
 
@@ -139,11 +139,11 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
     fname2 = "LogoByLyala.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @LaylaRobot")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @MrKoyomiBot")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @AwesomeSupport, {e}')
+   await event.reply(f'Error Report @KoyomiSupport, {e}')
 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
