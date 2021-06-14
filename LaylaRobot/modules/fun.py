81a0067b2b9842101fd22024ef250a2338ffac67
EvilzBots <85481867+EvilzBots@users.noreply.github.com>
2021-06-14 13:40:25 +0530

    Update fun.py

--- LaylaRobot/modules/fun.py
+++ LaylaRobot/modules/fun.py
@@ -345,20 +345,20 @@ def goodmorning(update, context):
     
 
 __help__ = """
- ❍ /runs*:* reply a random string from an array of replies
- ❍ /slap*:* slap a user, or get slapped if not a reply
- ❍ /shrug*:* get shrug XD
- ❍ /table*:* get flip/unflip :v
- ❍ /decide*:* Randomly answers yes/no/maybe
- ❍ /toss*:* Tosses A coin
- ❍ /bluetext*:* check urself :V
- ❍ /roll*:* Roll a dice
- ❍ /rlg*:* Join ears,nose,mouth and create an emo ;-;
- ❍ /shout <keyword>*:* write anything you want to give loud shout
- ❍ /weebify <text>*:* returns a weebified text
- ❍ /sanitize*:* always use this before /pat or any contact
- ❍ /pat*:* pats a user, or get patted
- ❍ /8ball*:* predicts using 8ball method 
+➤ /runs*:* reply a random string from an array of replies
+➤ /slap*:* slap a user, or get slapped if not a reply
+➤ /shrug*:* get shrug XD
+➤ /table*:* get flip/unflip :v
+➤ /decide*:* Randomly answers yes/no/maybe
+➤ /toss*:* Tosses A coin
+➤ /bluetext*:* check urself :V
+➤ /roll*:* Roll a dice
+➤ /rlg*:* Join ears,nose,mouth and create an emo ;-;
+➤ /shout <keyword>*:* write anything you want to give loud shout
+➤ /weebify <text>*:* returns a weebified text
+➤ /sanitize*:* always use this before /pat or any contact
+➤ /pat*:* pats a user, or get patted
+➤ /8ball*:* predicts using 8ball method 
 """
 
 SANITIZE_HANDLER = DisableAbleCommandHandler("sanitize", sanitize)
@@ -399,7 +399,7 @@ dispatcher.add_handler(TABLE_HANDLER)
 dispatcher.add_handler(GDMORNING_HANDLER)
 dispatcher.add_handler(GDNIGHT_HANDLER)
 
-__mod_name__ = "Memes"
+__mod_name__ = "Mᴇᴍᴇs"
 __command_list__ = [
     "runs",
     "slap",
