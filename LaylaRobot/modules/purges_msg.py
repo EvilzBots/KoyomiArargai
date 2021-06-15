6d24baa29c7ff24685b750269d719e4475a8d116
EvilzBots <85481867+EvilzBots@users.noreply.github.com>
2021-06-15 09:36:38 +0530

    Update purges_msg.py

--- LaylaRobot/modules/purges_msg.py
+++ LaylaRobot/modules/purges_msg.py
@@ -7,9 +7,9 @@ from telegram.ext import CommandHandler, Filters
 from telegram.ext.dispatcher import run_async
 from telegram.utils.helpers import mention_html
 
-from haruka import dispatcher, LOGGER
-from haruka.modules.helper_funcs.chat_status import user_admin, can_delete
-from haruka.modules.log_channel import loggable
+from LaylaRobot import dispatcher, LOGGER
+from LaylaRobot.modules.helper_funcs.chat_status import user_admin, can_delete
+from LaylaRobot.modules.log_channel import loggable
 
 
 @run_async
