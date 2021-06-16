4a71c19ee816c8f6d4a838272fa6709bf426aefe
EvilzBots <85481867+EvilzBots@users.noreply.github.com>
2021-06-16 20:56:35 +0530

    Create File_Tools.py

---
+++ LaylaRobot/modules/File_Tools.py
@@ -0,0 +1,6 @@
+__mod_name__ = "Fɪʟᴇ Tᴏᴏʟs"
+
+__help__ = """
+ ➤ `/zip`: reply to a telegram file to compress it in .zip format
+ ➤ `/unzip`: reply to a telegram file to decompress it from the .zip format
+"""
