405e2b413b0db27428d7d14d89214a67390ee18b
EvilzBots <85481867+EvilzBots@users.noreply.github.com>
2021-07-05 20:29:17 +0530

    Delete LaylaRobot/modules directory

--- LaylaRobot/modules/File_Tools.py
+++
@@ -1,6 +0,0 @@
-__mod_name__ = "Fɪʟᴇ Tᴏᴏʟs"
-
-__help__ = """
- ➤ `/zip`: reply to a telegram file to compress it in .zip format
- ➤ `/unzip`: reply to a telegram file to decompress it from the .zip format
-"""
